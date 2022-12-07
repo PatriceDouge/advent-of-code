635d4e27ce8c7f2f466446f83e4ba8047b07e4ec
Patrice Douge <patricedouge@Patrices-MacBook-Pro.local>
2022-12-07 08:35:31 -0500

    solved part 1

--- 2022/day1/main.go
+++ 2022/day1/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -24,9 +25,25 @@ func main() {
 
 	readFile.Close()
 
-	for _, line := range fileLines {
-		fmt.Println(line)
+	currentCal := 0
+	maxCal := 0
+
+	for _, num := range fileLines {
+
+		// convert input string to int, if space - error
+		cal, err := strconv.Atoi(num)
+
+		currentCal += cal
+
+		// if not empty, theres a space, check if currentCal > maxCal
+		if err != nil {
+			if currentCal > maxCal {
+				maxCal = currentCal
+			}
+			currentCal = 0
+		}
+
 	}
 
-	fmt.Println(fileLines)
+	fmt.Println(maxCal)
 }
