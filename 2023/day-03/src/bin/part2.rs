8fd0e786b457eab71f9c8dcf6f5c51738153ba0f
Patrice Douge <patricedouge@gmail.com>
2023-12-07 05:48:32 -0500

    day 3, part 2 done

--- 2023/day-03/src/bin/part2.rs
+++ 2023/day-03/src/bin/part2.rs
@@ -4,8 +4,81 @@ fn main() {
     println!("{}", output);
 }
 
-fn process(input: &str) -> String {
-    return "1".to_string();
+fn process(input: &str) -> u32 {
+    let mut gear_ratio_sum: Vec<u32> = Vec::new();
+    let mut grid: Vec<Vec<char>> = input
+        .lines()
+        .map(|line| line.chars().filter(|&c| c != ' ').collect())
+        .collect();
+
+    for row in 0..grid.len() {
+        for col in 0..grid[row].len() {
+            if grid[row][col] == '*' {
+                let gear_ratio = is_num_adjacent(&mut grid, row, col);
+                if gear_ratio > 0 {
+                    gear_ratio_sum.push(gear_ratio);
+                }
+            } else {
+                continue;
+            }
+        }
+    }
+    
+    return gear_ratio_sum.iter().sum::<u32>();
+}
+
+fn is_num_adjacent(grid: &mut Vec<Vec<char>>, row: usize, col: usize) -> u32 {
+    let mut gear_ratio: Vec<u32> = Vec::new();
+
+    if row > 0 && grid[row - 1][col].is_digit(10) {
+        let num = extract_number(grid, row - 1, col);
+        gear_ratio.push(num);
+    } if row > 0 && col > 0 && grid[row - 1][col - 1].is_digit(10) {
+        let num = extract_number(grid, row - 1, col - 1);
+        gear_ratio.push(num);
+    } if row > 0 && col < grid[row].len() - 1 && grid[row - 1][col + 1].is_digit(10) {
+        let num = extract_number(grid, row - 1, col + 1);
+        gear_ratio.push(num);
+    } if row < grid.len() - 1 && grid[row + 1][col].is_digit(10) {
+        let num = extract_number(grid, row + 1, col);
+        gear_ratio.push(num);
+    } if row < grid.len() - 1 && col > 0 && grid[row + 1][col - 1].is_digit(10) {
+        let num = extract_number(grid, row + 1, col - 1);
+        gear_ratio.push(num);
+    } if row < grid.len() - 1 && col <  grid[row].len() - 1 && grid[row + 1][col + 1].is_digit(10) {
+        let num = extract_number(grid, row + 1, col + 1);
+        gear_ratio.push(num);
+    } if col > 0 && grid[row][col - 1].is_digit(10) {
+        let num = extract_number(grid, row, col - 1);
+        gear_ratio.push(num);
+    } if col < grid[row].len() - 1 && grid[row][col + 1].is_digit(10) {
+        let num = extract_number(grid, row, col + 1);
+        gear_ratio.push(num);
+    }
+
+    dbg!(&gear_ratio);
+
+    if gear_ratio.len() == 2 {
+        return gear_ratio[0] * gear_ratio[1];
+    } else {
+        return 0;
+    }
+}
+
+fn extract_number(grid: &mut Vec<Vec<char>>, row: usize, col: usize) -> u32 {
+    let mut num_str = String::new();
+    let mut start_col = col;
+
+    while start_col > 0 && grid[row][start_col - 1].is_digit(10) {
+        start_col -= 1;
+    }
+
+    while start_col < grid[row].len() && grid[row][start_col].is_digit(10) {
+        num_str.push(grid[row][start_col]);
+        grid[row][start_col] = ' ';
+        start_col += 1;
+    }
+    return num_str.parse::<u32>().unwrap_or(0);
 }
 
 #[cfg(test)]
@@ -14,8 +87,17 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        let input = "";
-        assert_eq!("1", process(input));
+        let input = "467..114..
+        ...*......
+        ..35..633.
+        ......#...
+        617*......
+        .....+.58.
+        ..592.....
+        ......755.
+        ...$.*....
+        .664.598..";
+        assert_eq!(467835, process(input));
         Ok(())
     }
 }
